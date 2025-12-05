8da2e235dc41773e35ae3545bb2e1d86dbb13764
vovaDev <credo-planers-0i@icloud.com>
2025-12-05 13:05:42 +0200

    Initial Commit

---
+++ ProjectApp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectApp1
+//
+//  Created by Volodymyr Dziubenko on 05.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.brown)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectApp1/ProjectApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp1App.swift
+//  ProjectApp1
+//
+//  Created by Volodymyr Dziubenko on 05.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
